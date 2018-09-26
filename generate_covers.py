5a2c493d97631784629c4b35628adac0e3835a6b
s0hvaperuna <tariqharb54@gmail.com>
2018-09-26 22:48:16 +0300

    test

---
+++ generate_covers.py
@@ -0,0 +1,19 @@
+with open('Covers.css', 'r', encoding='utf-8') as f:
+    lines = f.read()
+
+covers = set()
+end = []
+for line in lines.split('\n'):
+    if not line.startswith('#xmenu'):
+        if line:
+            end.append(line)
+        continue
+
+    covers.add(line)
+
+with open('Covers2.css', 'w', encoding='utf-8') as f:
+    f.write('\n'.join(covers))
+    f.write('\n')
+    f.write('\n'.join(end))
+
+print('Now paste the contents of Covers2 to Covers to complete the process')
